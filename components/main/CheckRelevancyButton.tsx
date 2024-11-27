7e6a46a9553e1290132b10d0a117f4b373713f7e
Sindhur V Shabaraya <sindhur.shabaraya@contentstack.com>
2024-11-28 00:21:29 +0530

    fix client component bug

--- components/main/CheckRelevancyButton.tsx
+++ components/main/CheckRelevancyButton.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { Button } from "../ui/button"
 
 const CheckRelevancyButton = () => {
   const handleCheckRelevancy = () => {
     console.log("Check Relevancy");
   }
-  
+
   return (
     <Button onClick={handleCheckRelevancy}>Check Brand Relevancy</Button>
   )
